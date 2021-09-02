112294254d0cf40eb342b9d6b6d3de05aecc6051
Lenna <pampam32121@gmail.com>
2021-09-02 19:09:02 +0300

    first tasks completed - my answers

--- js/script.js
+++ js/script.js
@@ -14,6 +14,24 @@
 
 'use strict';
 
+//1) Удалить все рекламные блоки со страницы (правая часть сайта)
+const promoBanners = document.querySelectorAll('.promo__adv-banner');
+promoBanners.forEach( item => {
+    item.remove();
+});
+
+//2) Изменить жанр фильма, поменять "комедия" на "драма"
+const promoGenre = document.querySelector('.promo__genre');
+promoGenre.innerHTML = "драма";
+
+//3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+//Реализовать только при помощи JS
+const promoBG = document.querySelector('.promo__bg');
+promoBG.style.backgroundImage = "url('img/bg.jpg')";
+
+//4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+//Отсортировать их по алфавиту 
+//5) Добавить нумерацию выведенных фильмов
 const movieDB = {
     movies: [
         "Логан",
@@ -23,5 +41,11 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+const unsortedMovieDB = movieDB.movies;
+const sortedMovieDB = unsortedMovieDB.sort();
+console.log(sortedMovieDB);
 
-//1) Удалить все рекламные блоки со страницы (правая часть сайта)
+const interactiveItems = document.querySelectorAll('.promo__interactive-item');
+interactiveItems.forEach(function(item, i) {
+    item.innerHTML = `${i+1}. ${sortedMovieDB[i]}`;
+});
