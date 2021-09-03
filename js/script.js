d11ade8b1f1a02d494186c1472eba38f7fee87f0
Lenna <pampam32121@gmail.com>
2021-09-03 17:08:01 +0300

    Petrichenko's way

--- js/script.js
+++ js/script.js
@@ -15,19 +15,20 @@
 'use strict';
 
 //1) Удалить все рекламные блоки со страницы (правая часть сайта)
-const promoBanners = document.querySelectorAll('.promo__adv-banner');
-promoBanners.forEach( item => {
+const adv = document.querySelectorAll('.promo__adv img'),
+    bg = document.querySelector('.promo__bg'),
+    genre = bg.querySelector('.promo__genre');
+
+adv.forEach( item => {
     item.remove();
 });
 
 //2) Изменить жанр фильма, поменять "комедия" на "драма"
-const promoGenre = document.querySelector('.promo__genre');
-promoGenre.innerHTML = "драма";
+genre.textContent = "драма";
 
 //3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 //Реализовать только при помощи JS
-const promoBG = document.querySelector('.promo__bg');
-promoBG.style.backgroundImage = "url('img/bg.jpg')";
+bg.style.backgroundImage = "url('img/bg.jpg')";
 
 //4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 //Отсортировать их по алфавиту 
@@ -41,11 +42,27 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
-const unsortedMovieDB = movieDB.movies;
+
+//Мой способ
+/* const unsortedMovieDB = movieDB.movies;
 const sortedMovieDB = unsortedMovieDB.sort();
 console.log(sortedMovieDB);
 
 const interactiveItems = document.querySelectorAll('.promo__interactive-item');
-interactiveItems.forEach(function(item, i) {
+interactiveItems.forEach((item, i) => {
     item.innerHTML = `${i+1}. ${sortedMovieDB[i]}`;
+}); */
+
+//Способ автора курса
+const movieList = document.querySelector('.promo__interactive-list');
+movieList.innerHTML = "";
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i+1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `;
 });
+
